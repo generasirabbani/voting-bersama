64d12aeb7c31e95b562663138f49ef0607b93dc1
dwputraa0 <92132505+dwputraa0@users.noreply.github.com>
2022-12-18 10:09:23 -0800

    Add files via upload

--- beranda.php
+++ beranda.php
@@ -1,6 +1,7 @@
 <?php
     include("config.php");
     session_start();
+    ob_start();
     $data = pg_query("SELECT * from pengguna where email = '".$_SESSION['email']."'");
     $data2 = pg_query("SELECT * from admin where token = '".$_SESSION['token']."'");
     $hehe = pg_fetch_array($data);
@@ -16,58 +17,84 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beranda</title>
-    <link rel="stylesheet" href="beranda.css">
-    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <title>E-Voting | Beranda</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <link rel="stylesheet" href="beranda.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'> -->
 </head>
-<body>
+<body class="bg-gray-100">
     <header>
-    <div class="main">
-        <ul class="navbar">
-                <li><a href="beranda.php" class="dropdown">Home</a></li>
-                <li><a href="livecount.php" class="tombolLogout">Live Count</a></li>
-        </ul>
-        <p>|</p>
-        <a href="logout.php" class="tombolLogout">Log out</a>
-	</div>
+    <div class="main w-full bg-sky-900 flex justify-between p-5 text-xl text-slate-400 shadow-xl fixed">
+		<div class="navbar flex">
+			<div class="pl-12 hover:text-slate-100 duration-300"><a href="beranda.php" class="dropdown">Home</a></div>
+            <div class="px-10 hover:text-slate-100 duration-300"><a href="livecount.php">Live Count</a></div>
+        </div>
+        <div class="main">
+            <a href="logout.php" class="tombolLogout pr-16 hover:text-slate-100 duration-300">Log out</a>
+		</div>
+    </div>
 	</header>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    
+    <?php
+    $lines = pg_fetch_array(pg_query("SELECT count(*) from kandidat where token = '".$_SESSION['token']."'"));
+    ?>
+
+    <div class="h-44"></div>
+    <?php if($lines[0]==2){ ?>
+        <div class="mx-60 flex justify-between">
+    <?php } elseif($lines[0]==3){ ?>
+        <div class="mx-24 flex justify-between">
+    <?php }else{ ?>
+        <!-- <div class="grid gap-8 grid-cols-3"> -->
+    <?php } ?>
+
     <?php
     $i = 1;
     $query = pg_query("SELECT * from kandidat where token = '".$_SESSION['token']."' order by nama asc");
-    while($kandidat = pg_fetch_array($query)){;?>
-        <p><?php echo $i?></p>
-        <p><?php echo $kandidat['nama']?></p>
-        <a href="<?php echo $kandidat['link']?>"><?php echo $kandidat['link']?></a>
-        <p><?php echo $kandidat['visi']?></p>
-        <p><?php echo $kandidat['misi']?></p>
+    while($kandidat = pg_fetch_array($query)){;
+        $tes[$i] = $kandidat['idkandidat']?>
+        <div class="w-96 rounded shadow-lg h-min">
+            <div class="w-full text-8xl h-60 bg-slate-300 rounded-t pt-16">
+                <div class="rounded-full bg-slate-600 w-28 h-28 text-center mx-auto align-middle text-slate-100"><?php echo $i?></div>
+            </div>
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">
+                    <?php echo $kandidat['nama']?>
+                </div>
+                    <a href="<?php echo $kandidat['link']?>" class="cpt-3"><?php echo $kandidat['link']?></a>
+                    <p class="pt-3"><?php echo $kandidat['visi']?></p>
+                    <p class="py-3"><?php echo $kandidat['misi']?></p>
+            </div>
         <?php
         if($_SESSION['cek'] == 'f'){;?>
-        <div>
-        <form action = "" method = "POST">
-        <input type="submit" name="pilih" class="login login-submit" value="Pilih">
-        </form>
+            <div class="">
+                <form action = "" class="py-3"   method = "POST">
+                <input type="submit" class="px-8 py-2 rounded bg-sky-600 ml-36 rounded-full text-slate-100 hover:bg-sky-700 duration-100" name="pilih[<?php echo $i; ?>]" class="login login-submit" value="Pilih">
+                </form>
+            </div>
         </div>
         <?php 
-        }
-            if(isset($_POST['pilih'])&&!empty($_POST['pilih'])){
-                $ID = $kandidat['idkandidat'];
-                $query = pg_query("UPDATE kandidat SET jumlahsuara= jumlahsuara + 1 WHERE idkandidat='$ID'");
-                $query1 = pg_query("UPDATE pengguna SET cek = 'TRUE' WHERE email= '".$_SESSION['email']."'");
-                $query3 = pg_query("UPDATE pengguna SET pilih = '$ID' WHERE email= '".$_SESSION['email']."'");
-                $query2 = pg_query("Select * from pengguna WHERE email= '".$_SESSION['email']."'");
-                $_SESSION['cek'] = pg_fetch_array($query2)['cek']; 
-                if($query && $query1 && $query3){
-                    header('Location: beranda.php');
-                }
-                else{
-                    die("gagal menghapus...");
+        } else { ?>
+            </div>
+        <?php }
+            $i++;};
+            for($j = 1; $j < $i; $j++){
+                $pil = "pilih[$j]";
+                if(isset($_POST["pilih"][$j])&&!empty($_POST['pilih'][$j])){
+                    $ID = $tes[$j];
+                    $query = pg_query("UPDATE kandidat SET jumlahsuara= jumlahsuara + 1 WHERE idkandidat='$ID'");
+                    $query1 = pg_query("UPDATE pengguna SET cek = 'TRUE' WHERE email= '".$_SESSION['email']."'");
+                    $query3 = pg_query("UPDATE pengguna SET pilih = '$ID' WHERE email= '".$_SESSION['email']."'");
+                    $query2 = pg_query("Select * from pengguna WHERE email= '".$_SESSION['email']."'");
+                    $_SESSION['cek'] = pg_fetch_array($query2)['cek']; 
+                    if($query && $query1 && $query3){
+                        header('Location: beranda.php');
+                    }
+                    else{
+                        die("gagal menghapus...");
+                    }
                 }
             }
-            $i++;};
         ?>
+        <div class="h-32 bg-red"></div>
 </body>
